2a886d477771345a4c5ddc28663f3566f1954463
Adrian PK <adrianpk@adrianpk.com>
2019-06-22 19:27:17 +0200

    Function and methods moved to package main file.

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -11,3 +11,108 @@ func NewRabbitMQ(ctx context.Context, cfg *Config, log *log.Logger) (*RabbitMQ, 
 	r, err := newRabbitMQ(ctx, cfg, log)
 	return r, err
 }
+
+// newRabbitMQ create a new RabbitMQ broker handler.
+func newRabbitMQ(ctx context.Context, cfg *Config, log *log.Logger) (*RabbitMQ, error) {
+	r := &RabbitMQ{
+		// Generic
+		ctx:       ctx,
+		cfg:       cfg,
+		name:      cfg.Name,
+		ready:     false,
+		alive:     false,
+		log:       log,
+		Listeners: make(map[string]*Listener),
+		Emitters:  make(map[string]*Emitter),
+	}
+
+	r.conn = <-r.RetryConnection(cfg)
+
+	return r, nil
+}
+
+// RabbitMQURL returns a RabbitMQ connection URL.
+func (c *Config) RabbitMQURL() string {
+	panic("TODO: Not implemented yet")
+}
+
+// SetConfig for RabbitMQ client.
+func (r *RabbitMQ) SetConfig(cfg *Config) {
+	r.cfg = cfg
+}
+
+// Connect to RabbitMQ.
+func (r *RabbitMQ) Connect(retry bool) error {
+	if r.cfg == nil {
+		return errors.New("setup a configuration before connect")
+	}
+	if retry {
+		r.conn = <-r.RetryConnection(r.cfg)
+	}
+
+	var err error
+	r.conn, err = r.Connection()
+	return err
+}
+
+// ConnStatus returns true if broker
+// connection is open.
+func (r *RabbitMQ) ConnStatus() bool {
+	return !r.conn.IsClosed()
+}
+
+// AddListener to the broker
+func (r *RabbitMQ) AddListener(name, exchange, queue string) error {
+	l, err := r.NewListener(exchange, queue)
+	if err != nil {
+		return err
+	}
+	r.Listeners[name] = l
+	return nil
+}
+
+// AddEmitter to the brokergo i
+// queue parameter is optional but if it is provided
+// A queue and binding to the exchange will be created
+// for each provided name.
+func (r *RabbitMQ) AddEmitter(name, exchange string, queue ...string) error {
+	if len(queue) < 1 {
+		queue = []string{""} //TODO: Implement (multiple) queue binding
+	}
+
+	e, err := r.NewEmitter(exchange, queue[0])
+	if err != nil {
+		return err
+	}
+
+	r.Emitters[name] = e
+	return nil
+}
+
+// NewListener returns a new RabbitMQ broker listener.
+func (r *RabbitMQ) NewListener(exchange, queue string) (*Listener, error) {
+	if r.conn == nil {
+		return nil, errors.New("broker has no connection")
+	}
+
+	return &Listener{
+		connection: r.conn,
+		exchange:   exchange,
+		queue:      queue,
+		mapper:     mapper.NewMessageMapper(),
+		log:        r.log,
+	}, nil
+}
+
+// NewEmitter returns a new RabbitMQ broker emitter.
+func (r *RabbitMQ) NewEmitter(exchange, queue string) (*Emitter, error) {
+	if r.conn == nil {
+		return nil, errors.New("broker has no connection")
+	}
+
+	return &Emitter{
+		connection: r.conn,
+		exchange:   exchange,
+		log:        r.log,
+	}, nil
+}
